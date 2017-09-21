26fba7d572112ca11f7a07041ced3705ca1387ea
denden-kata <denden3104@gmail.com>
2017-09-21 17:01:00 +0900

    別の書き方メモ（addEventListener）

--- main.js
+++ main.js
@@ -10,8 +10,18 @@
 // これらの２つを無視して処理したい
 // jQueryとか使うと簡単に書けたりしないですかね（願望）
 
+
+// 画面遷移時にアラートを表示する
 window.onbeforeunload = function(){
-    
     
     return 0;
 }
+
+
+
+/*  -----  sample1  -----  */
+// こっちの書き方はよく知らない (addEventListener)
+/*
+window.addEventListener('beforeunload', function(e){
+});
+*/
